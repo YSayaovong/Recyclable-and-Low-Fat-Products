6eaecc3bf48b6528fa6abb10bcf512b0c493a3bb
YSayaovong <110697608+YSayaovong@users.noreply.github.com>
2024-08-05 23:30:11 -0500

    Time: 50 ms (66.66%), Space: 48.9 MB (34.08%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -1,7 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
 var map = function(arr, fn) {
-  const transformedArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    transformedArr[i] = fn(arr[i], i);
+  var mappedArray = [];
+  for (var i = 0; i < arr.length; i++) {
+    mappedArray.push(fn(arr[i], i));
   }
-  return transformedArr;
+  return mappedArray;
 };
