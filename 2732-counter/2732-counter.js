915f7d84fdda044d2e1aa7fb575dc63fee48fc41
YSayaovong <110697608+YSayaovong@users.noreply.github.com>
2024-08-19 17:56:13 -0500

    Time: 53 ms (41.7%), Space: 48.7 MB (74.35%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -1,6 +1,16 @@
-function createCounter(start) {
-  let count = start;
-  return function() {
-    return count++;
-  }
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    }
 }
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
