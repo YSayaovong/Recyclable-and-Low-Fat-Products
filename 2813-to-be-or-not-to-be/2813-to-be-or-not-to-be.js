93f248434c781c8bdacd68aa9279b386c416a938
YSayaovong <110697608+YSayaovong@users.noreply.github.com>
2024-08-19 18:00:20 -0500

    Time: 44 ms (91.43%), Space: 48.8 MB (61.75%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -5,12 +5,19 @@
 var expect = function(val) {
     return {
         toBe: (val2) => {
-            if(val === val2) return true;
-            else throw new Error("Not Equal");
+            if (val !== val2){
+                throw new Error("Not Equal");
+            }else{
+                return true;
+            }
         },
         notToBe: (val2) => {
-            if(val !== val2) return true;
-            else throw new Error("Equal");
+            if (val === val2){
+                throw new Error("Equal");
+            }
+            else{
+                return true;
+            }
         }
     }
 };
