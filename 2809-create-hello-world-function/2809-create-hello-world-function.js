1052212bfb86247ed998a4fddf0d084541768aaa
YSayaovong <110697608+YSayaovong@users.noreply.github.com>
2024-08-19 17:51:15 -0500

    Time: 53 ms (39.4%), Space: 48.8 MB (29.43%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,7 +1,5 @@
-function createHelloWorld() {
-  const greeting = "Hello World";
-  
-  return function() {
-    return greeting;
-  };
+var createHelloWorld = function() { 
+    return function() {
+        return "Hello World";
+    }
 }
